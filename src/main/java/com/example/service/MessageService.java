8a7cba61f3dffaba83a2b7002f4a8905abd1c545
speedy mcfan <anandmure@gmail.com>
2024-07-07 19:51:29 +0000

    MessageService Skeleton

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -4,6 +4,7 @@ import com.example.entity.*;
 import com.example.repository.*;
 
 import javax.transaction.Transactional;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,29 @@ public class MessageService {
     @Autowired
     public MessageService(MessageRepository messageRepository){
         this.messageRepository = messageRepository;
+    }
+
+    public Message getAllMessages(){
+        return null;
+    }
+
+    public Message getMessageById(int messageId){
+        return null;
+    }
+
+    public Message createMessage(Message message){
+        return null;
+    }
+
+    public Message deleteMessage(int messageId){
+        return null;
+    }
+
+    public Message updateMessage(int messageId){
+        return null;
+    }
+
+    public List<Message> allMessagesByAccount(int accountId){
+        return null;
     }
 }
